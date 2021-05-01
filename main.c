1865fe631b3f3a19617c0831480255eb554a6682
Alfredo Martins <alfredomartins624@gmail.com>
2021-05-01 15:59:54 +0100

    Actualização das pastas.

--- main.c
+++ main.c
@@ -4,7 +4,7 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-	
+	printf("Hello!\n");
 	system("pause");
 	return 0;
 }
