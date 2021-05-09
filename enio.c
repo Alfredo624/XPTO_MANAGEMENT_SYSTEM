682383910176830ca6d19ebb190a97e72e90aec9
datwayskrt <enioblvck@gmail.com>
2021-05-09 12:55:03 +0100

    prototipo da func insert

--- enio.c
+++ enio.c
@@ -1,6 +1,39 @@
 #include<stdio.h>
+#include<stdlib.h>
 
-int main(){
-    return 0;
-    printf("just testing the new git ssh connection");
+//passando o codigo da insercao e o caminho do ficheiro
+int insert(int cod, char* path)
+{   
+	//declaracao das variaveis
+	FILE *file;
+    char user_name[30], user_password[12];
+    //insercao por codigo
+    switch (cod)
+    {
+        case 1:
+        //abertura do ficheiro
+    	file = fopen(path,"a+");
+        puts("Digite o nome do usuario:");
+        gets(user_name);
+        puts("Digite a senha:");
+        gets(user_password);
+        //insercao no ficheiro
+        fprintf(file, "\n%s %s",user_name,user_password);
+		fclose(file);
+            return 1;
+    
+        default:
+            return 0;
+    }
 }
+int main(){
+   
+    char word[101]="data\\employee_user.dat";
+	if(insert(1,word))
+		puts("usuario inserido com sucesso!");
+	else
+		puts("algo deu errado :(");
+    return 0;
+    
+}
+
